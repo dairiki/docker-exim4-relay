f27b548eaa48b4a4521ef2d4f19a83f1c4d09143
Jeff Dairiki <dairiki@dairiki.org>
2020-09-15 09:58:33 -0700

    Support setting dc_relay_nets via docker env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,14 @@ if [ "$1" = 'exim' ]; then
         echo "$ETC_MAILNAME" > /etc/mailname
     fi
 
-    # update-exim4.conf -v
+    if [ -n "$RELAY_NETS" ]
+    then
+        set -x
+        sed -i "s!^\\(dc_relay_nets=\\).*!\\1'$RELAY_NETS'!" \
+            /etc/exim4/update-exim4.conf.conf
+    fi
+
+    update-exim4.conf -v
 
     if [ "$(id -u)" = '0' ]; then
         mkdir -p /var/spool/exim4 /var/log/exim4 || :
